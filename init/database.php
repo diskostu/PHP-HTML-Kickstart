afb40e09e98ea764129d93998ef3b99a72038a67
Alec Ritson <alecritson@gmail.com>
2012-05-25 10:44:53 +0100

    Added database constants for connection

---
+++ init/database.php
@@ -0,0 +1,7 @@
+
+<?php
+define('DBHOST', 'localhost'); // Host on which your DB is located (usually kept localhost)
+define('DBUSER', 'aritson'); // Your database user password (what you use for phpMyAdmin)
+define('DBPASSWORD', ''); // Your user password for the database
+define('DB', 'portfolio') // The database you want to connect to.
+?>
