7ae7e0b6c068fcdaef0122b70c54c1b56e453b2e
Alec Ritson <alecritson@gmail.com>
2012-05-25 10:44:09 +0100

    Changed init location on boilerplate

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <?php
-	require_once('../init/base.php');
+	include('init/base.php');
 ?>
 <!DOCTYPE HTML>
 <html lang="en-US">
@@ -9,12 +9,13 @@
 	<title>website title</title>
 	<link rel="stylesheet" href="<?php echo BASE ?>css/reset.css">
 	<link rel="stylesheet" href="<?php echo BASE ?>css/style.css">
+	<link rel="stylesheet" href="<?php echo BASE ?>css/boilerplate.css">
 	<!--[if IE]>
 		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
 </head>
 <body>
 	<header>
-		<h1>Header</h1>
+		<h1>PHP-HTML5 Boilerplate</h1>
 	</header>
 	<div class="container" id="main">
